d6a12066249440dd1d8cf95d16bf07135e4576cb
kazucmpt <w88kazu88w@gmail.com>
2019-04-05 09:58:05 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -11,12 +11,22 @@ class DataSet:
 		for i in range(num_data):
 			self.y[i] = self.make_y(self.x[i], noise_level)
 
+		self.x, self.y = self.dual_sort(self.x, self.y)
+
 	@staticmethod
 	def make_y(x, noise_level):
 			if x > 0:
 				return 1.0/2.0*x**(1.2) + math.cos(x) + np.random.normal(0, noise_level)
 			else:
 				return math.sin(x) + 1.1**x + np.random.normal(0, noise_level)
+
+	@staticmethod
+	def dual_sort(p, q):
+		tmp = np.argsort(p)
+		q = q[tmp]
+		p = np.sort(p)
+
+		return p, q
 
 	def plot(self):
 		plt.scatter(self.x, self.y)
