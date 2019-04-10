deccc391f7d078e83a7f31cabbb80a1f9118e6f7
kazucmpt <w88kazu88w@gmail.com>
2019-04-10 20:18:51 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -8,12 +8,11 @@ class DataSet:
 		self.xmin = xmin
 		self.xmax = xmax
 		self.x = (xmax - xmin) * np.random.rand(num_data) + xmin
+		self.x = np.sort(self.x)
+		
 		self.y = np.empty(num_data)
-
 		for i in range(num_data):
 			self.y[i] = self.make_y(self.x[i], noise_level)
-
-		self.x, self.y = self.dual_sort(self.x, self.y)
 
 	@staticmethod
 	def make_y(x, noise_level):
@@ -21,14 +20,6 @@ class DataSet:
 				return 1.0/2.0*x**(1.2) + math.cos(x) + np.random.normal(0, noise_level)
 			else:
 				return math.sin(x) + 1.1**x + np.random.normal(0, noise_level)
-
-	@staticmethod
-	def dual_sort(p, q):
-		tmp = np.argsort(p)
-		q = q[tmp]
-		p = np.sort(p)
-
-		return p, q
 
 	def plot(self):	
 		plt.figure(figsize=(15, 10))
